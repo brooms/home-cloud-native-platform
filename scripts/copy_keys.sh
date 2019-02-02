fb3f07c9d0437ff5e0d212749dbe90c0d92f84fa
philip <greenthegarden@gmail.com>
2019-02-02 21:42:27 +1030

    add default values to inputs

--- scripts/copy_keys.sh
+++ scripts/copy_keys.sh
@@ -1,18 +1,25 @@
 #!/usr/bin/env bash
 
-FILE="id_rsa_hcnp"
-DIR="${HOME}/.ssh"
-REMOTE_USER=${USER}
+key_file="id_rsa_hcnp"
+key_file_dir="${HOME}/.ssh"
+remote_user=${USER}
 
-echo "Enter remote host name:"
-read remote_host
+read -p "Enter key file name [${key_file}]: " key_file_input
+key_file=${key_file_input:-${key_file}}
 
-echo "Enter remote user:"
-read remote_user
+read -p "Enter key file directory [${key_file_dir}]: " key_file_dir_input
+key_file_dir=${key_file_dir_input:-${key_file_dir}}
 
+read -p "Enter remote host [localhost]: " remote_host_input
+remote_host=${remote_host_input:-localhost}
 
-if [ -f ${DIR}/${FILE} ] ; then
-  ssh-copy-id -i ${DIR}/${FILE} ${remote_user}@${remote_host}
+read -p "Enter remote user [${remote_user}]: " remote_user_input
+remote_user=${remote_user_input:-${remote_user}}
+
+if [ -f ${key_file_dir}/${key_file} ] ; then
+  echo "copying key file ${key_file_dir}/${key_file} to ${remote_user}@${remote_host}"
+  ssh-copy-id -i ${key_file_dir}/${key_file} ${remote_user}@${remote_host}
 else
-  echo "ssh key file ${DIR}/${FILE} not found!"
+  echo "key file ${key_file_dir}/${key_file} not found!"
+  exit
 fi
--- scripts/create_keys.sh
+++ scripts/create_keys.sh
@@ -1,10 +1,16 @@
 #!/usr/bin/env bash
 
 # see https://www.ssh.com/ssh/keygen for details
-ALGORITHM="rsa" # one of rsa, dsa, ecdsa, ed25519
-KEY_SIZE="4096"
-COMMENT="home-cloud-native-platform"
-FILE="id_${ALGORITHM}_hcnp"
-DIR="${HOME}/.ssh"
+key_algorithm="rsa" # one of rsa, dsa, ecdsa, ed25519
+key_size="4096"
+key_comment="home-cloud-native-platform"
+key_file="id_${ALGORITHM}_hcnp"
+key_file_dir="${HOME}/.ssh"
 
-ssh-keygen -t ${ALGORITHM} -b ${KEY_SIZE} -C ${COMMENT} -f ${DIR}/${FILE}
+if [ -f ${key_file_dir}/${key_file} ] ; then
+  echo "key file ${key_file_dir}/${key_file} already exists"
+  exit
+else
+  echo "generating key file ${key_file_dir}/${key_file} using ${key_algorithm} algorithm with size ${key_size}"
+  ssh-keygen -t ${key_algorithm} -b ${key_size} -C ${key_comment} -f ${key_file_dir}/${key_file}
+fi
